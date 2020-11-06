3172ed95eb880ea25823cd392e01382f2f707352
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-11-06 07:53:58 +0530

    layout and burger builder implemented in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Layout from './components/layout/layout';
+import BurgerBuilder from './containers/burger_builder/burger_builder';
 
 function App() {
   return (
     <div>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     </div>
   );
 }
