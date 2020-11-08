f77a17ce33dd289fab4f99127e2ff740df24ac2f
creanick <creanickofficial@gmail.com>
2020-11-08 14:58:08 +0530

    basic backdrop created

---
+++ src/components/backdrop/backdrop.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const BackDrop = styled.div`
+    width: 100%;
+    height: 100%;
+    background: black;
+    opacity: 0.5;
+`;
+export default BackDrop;
--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import BackDrop from '../backdrop/backdrop';
 
 const Modal:React.FunctionComponent<{}> = (props)=>{
     return (
@@ -19,12 +20,6 @@ const Wrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-`;
-const BackDrop = styled.div`
-    width: 100%;
-    height: 100%;
-    background: black;
-    opacity: 0.5;
 `;
 const Container = styled.div`
     padding: 20px 20px;
