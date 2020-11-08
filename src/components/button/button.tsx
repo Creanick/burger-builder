6f645337afa0ba0f9cc08e3161916af7e3eeac1d
creanick <creanickofficial@gmail.com>
2020-11-08 13:48:49 +0530

    button compoenent created

---
+++ src/components/button/button.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    color: white;
+    background-color: ${props=>props.color || 'grey'};
+    margin: auto;
+    outline: none;
+    &:hover{
+        cursor: pointer;
+    }
+    &:active{
+        transform: scale(0.9);
+    }
+    &:disabled{
+        background-color: lightgrey;
+        cursor: not-allowed;
+    }
+`;
+
+
+export default Button;
