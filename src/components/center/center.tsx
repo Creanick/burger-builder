3eabf63489d09f1dc571aaaf764183c5a12b845d
creanick <creanickofficial@gmail.com>
2020-11-08 13:08:10 +0530

    center component created

---
+++ src/components/center/center.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Center = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    margin: auto;
+`;
+
+export default Center;
