fadf6b6c67334b1499763db52b931fc834dae174
creanick <creanickofficial@gmail.com>
2020-11-08 09:42:18 +0530

    updated ingredient prices

--- src/data/ingredient_hub.ts
+++ src/data/ingredient_hub.ts
@@ -10,10 +10,10 @@ interface IIngredientHub{
     [index:string]:{label:string,price:number,type:IngredientType}
 }
 const ingredientHub:IIngredientHub = {
-    [IngredientType.meat]:{label: "Meat",price: 34,type:IngredientType.meat},
-    [IngredientType.cheese]:{label: "Cheese",price: 10,type:IngredientType.cheese},
-    [IngredientType.salad]:{label: "Salad",price: 2,type:IngredientType.salad},
-    [IngredientType.bacon]:{label: "Bacon",price: 89,type:IngredientType.bacon}
+    [IngredientType.meat]:{label: "Meat",price: 34.45,type:IngredientType.meat},
+    [IngredientType.cheese]:{label: "Cheese",price: 10.32,type:IngredientType.cheese},
+    [IngredientType.salad]:{label: "Salad",price: 2.7,type:IngredientType.salad},
+    [IngredientType.bacon]:{label: "Bacon",price: 89.2,type:IngredientType.bacon}
 }
 
 export default ingredientHub;
