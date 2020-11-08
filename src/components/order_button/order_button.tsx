8af47b0398ca4ae76d7af9cec0b9d5a07e499be9
creanick <creanickofficial@gmail.com>
2020-11-08 13:11:21 +0530

    order button created

---
+++ src/components/order_button/order_button.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const OrderButton = styled.button`
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    color: white;
+    background-color: orange;
+    margin: auto;
+    outline: none;
+    &:hover{
+        cursor: pointer;
+    }
+    &:active{
+        transform: scale(0.9);
+    }
+    &:disabled{
+        background-color: #fad693;
+    }
+`;
+
+export default OrderButton;
