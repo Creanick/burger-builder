8431ae87909e9ba9fe4939a731799c383c0cb2ce
creanick <creanickofficial@gmail.com>
2020-11-08 13:51:26 +0530

    changed from control button to button

--- src/components/burger_controls/burger_controls.tsx
+++ src/components/burger_controls/burger_controls.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import ingredientHub, { IngredientType } from '../../data/ingredient_hub';
+import Button from '../button/button';
 interface Props{
     onAddIngredient?:(type:IngredientType)=>void
 }
@@ -9,11 +10,13 @@ const BurgerControls:React.FunctionComponent<Props> = ({onAddIngredient})=>{
     for (const key in ingredientHub) {
         const ingredient = ingredientHub[key];
         controlButtons.push(
-        <ControlButton 
+        <Button
+        color="green" 
+        style={{margin:"10px"}}
         onClick={()=>onAddIngredient && onAddIngredient(ingredient.type)}
          key={ingredient.label}>
         {ingredient.label} +
-        </ControlButton>
+        </Button>
         );
     }
     return (
@@ -28,20 +31,4 @@ const Container = styled.div`
     justify-content: center;
 `;
 
-const ControlButton = styled.button`
-    border: none;
-    margin: 10px;
-    padding: 10px 20px;
-    background-color: seagreen;
-    color: white;
-    border-radius: 4px;
-    outline: none;
-    &:hover{
-        cursor: pointer;
-        background-color: green;
-    }
-    &:active{
-        transform: scale(0.9);
-    }
-`;
 export default BurgerControls;
