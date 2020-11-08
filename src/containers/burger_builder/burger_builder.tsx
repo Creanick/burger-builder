a228043457d2473e4e8f5def68701baf0a6add80
creanick <creanickofficial@gmail.com>
2020-11-08 09:42:29 +0530

    added price state

--- src/containers/burger_builder/burger_builder.tsx
+++ src/containers/burger_builder/burger_builder.tsx
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import Burger from '../../components/burger/burger';
 import BurgerControls from '../../components/burger_controls/burger_controls';
-import { IngredientType } from '../../data/ingredient_hub';
+import ingredientHub, { IngredientType } from '../../data/ingredient_hub';
 
 interface State{
-    ingredients: IngredientType[]
+    ingredients: IngredientType[],
+    totalPrice: number
 }
 class BurgerBuilder extends Component<{},State> {
     state = {
-        ingredients:[
-            IngredientType.bacon,
-            IngredientType.cheese,
-        ]
+        ingredients:[],
+        totalPrice: 0
     }
     render() {
         return (
@@ -20,20 +19,26 @@ class BurgerBuilder extends Component<{},State> {
                 onIngredientClick={this.ingredientRemoveHandler}
                 ingredients={this.state.ingredients}/>
                 <BurgerControls onAddIngredient={this.ingredientAddingHandler}/>
+                <p>Total Price : {this.state.totalPrice.toFixed(2)}</p>
             </div>
         );
     }
     ingredientAddingHandler = (type:IngredientType)=>{
         this.setState((state)=>{
-            return {ingredients:[...state.ingredients,type]}
+            const price = ingredientHub[type].price;
+            const totalPrice = state.totalPrice + price;
+            return {ingredients:[...state.ingredients,type],
+                totalPrice: totalPrice};
         });
     }
     ingredientRemoveHandler = (index:number)=>{
         if(index >= 0 && index < this.state.ingredients.length){
             this.setState(state=>{
-                const ingredients = [...state.ingredients];
-                ingredients.splice(index,1);
-                return {ingredients: ingredients};
+                const ingredients = [...state.ingredients]; 
+                const type = ingredients.splice(index,1);
+                const price = ingredientHub[type[0]].price;
+            const totalPrice = state.totalPrice - price;
+                return {ingredients: ingredients,totalPrice:totalPrice};
             })
         }
     }
