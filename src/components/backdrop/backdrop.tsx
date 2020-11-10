56232f392bdd280f9c9ac23cd32962c07bbdd2b2
creanick <creanickofficial@gmail.com>
2020-11-10 11:51:59 +0530

    changed backdrop to initial

--- src/components/backdrop/backdrop.tsx
+++ src/components/backdrop/backdrop.tsx
@@ -1,17 +1,9 @@
-import React from 'react';
 import styled from "styled-components";
 
-const Container = styled.div`
+const BackDrop = styled.div`
     width: 100%;
     height: 100%;
     background: black;
     opacity: 0.5;
 `;
-
-interface Props{
-    onClick?:()=>void
-}
-const BackDrop:React.FunctionComponent<Props> = (props)=>(
-    <Container onClick={props.onClick}/>
-);
 export default BackDrop;
