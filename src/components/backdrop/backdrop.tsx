4c54c61c2de26d504cde1d649fc5a0821b75e78b
creanick <creanickofficial@gmail.com>
2020-11-10 09:57:34 +0530

    onclick handler added in backdrop

--- src/components/backdrop/backdrop.tsx
+++ src/components/backdrop/backdrop.tsx
@@ -1,9 +1,17 @@
+import React from 'react';
 import styled from "styled-components";
 
-const BackDrop = styled.div`
+const Container = styled.div`
     width: 100%;
     height: 100%;
     background: black;
     opacity: 0.5;
 `;
+
+interface Props{
+    onClick?:()=>void
+}
+const BackDrop:React.FunctionComponent<Props> = (props)=>(
+    <Container onClick={props.onClick}/>
+);
 export default BackDrop;
