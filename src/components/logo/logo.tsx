eeb1b0cf0f08a456abf63f2d72ae6f90ca47dba4
creanick <creanickofficial@gmail.com>
2020-11-12 11:56:07 +0530

    logo created

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import logoImg from '../../assets/images/burger_logo.png';
+const Container = styled.div`
+    padding: 6px;
+    background-color: white;
+    border-radius: 4px;
+    height: 100%;
+    img{
+        height: 100%;
+        display: block;
+    }
+`;
+const Logo:React.FunctionComponent = (props)=>{
+    return (
+        <Container>
+            <img src={logoImg} alt="Burger Logo"/>
+        </Container>
+    );
+}
+
+export default Logo;
--- src/components/navigation/toolbar/toolbar.tsx
+++ src/components/navigation/toolbar/toolbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Logo from '../../logo/logo';
 const Container = styled.div`
     height: 56px;
     background-color: brown;
@@ -13,11 +14,17 @@ const Container = styled.div`
     top: 0;
     z-index: 90;
 `;
+const Padding = styled.div`
+    height: 100%;
+    padding: 6px;
+`;
 const Toolbar:React.FunctionComponent<{}> = (props)=>{
     return (
         <Container>
             <div>Menu</div>
-            <div>Logo</div>
+            <Padding>
+                <Logo/>
+            </Padding>
             <nav>Nav</nav>
         </Container>
     );
