2e21ea46f5460793f7527461a2d26b8497abe9bc
creanick <creanickofficial@gmail.com>
2020-11-28 13:28:30 +0530

    created private backdro for side drawer

--- src/components/side_drawer/side_drawer.tsx
+++ src/components/side_drawer/side_drawer.tsx
@@ -1,13 +1,9 @@
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
-import BackDrop from '../backdrop/backdrop';
 import Logo from '../logo/logo';
 import Nav from '../navigation/toolbar/nav/nav';
 
-interface SideDrawerProps{
-    show?: boolean;
-}
-const Wrapper = styled.div<SideDrawerProps>`
+const Wrapper = styled.div<{show?:boolean}>`
     position: fixed;
     left: 0;
     top:0;
@@ -17,15 +13,34 @@ const Wrapper = styled.div<SideDrawerProps>`
     background-color: white;
     z-index: 200;
     padding: 16px;
-
+    transition: transform 0.3s ease-in;
+    transform: ${props=>props.show?"translateX(0)":"translateX(-100%)"};
     @media (min-width: 500px){
         display: none;
     }
 `;
-const SideDrawer:React.FunctionComponent = (props)=>(
+const BackDrop = styled.div<{show?:boolean}>`
+    width: 100%;
+    height: 100%;
+    background: black;
+    opacity: 0.5;
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 100;
+    display: ${props=>props.show?"block":"none"};
+    @media (min-width: 500px){
+        display: none;
+    }
+`;
+interface SideDrawerProps{
+    show?: boolean;
+    onBackClick?:()=>void
+}
+const SideDrawer:React.FunctionComponent<SideDrawerProps> = ({show=false,onBackClick})=>(
     <Fragment>
-        <BackDrop show onClick={()=>console.log("clicked drop")}/>
-        <Wrapper>
+        <BackDrop show={show} onClick={onBackClick}/>
+        <Wrapper show={show}>
             <div style={{height: "6%"}}>
             <Logo/>
             </div>
