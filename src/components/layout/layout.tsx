1e5aa2dcb942c157311932e498e7de007111aa52
creanick <creanickofficial@gmail.com>
2020-11-28 11:51:40 +0530

    basic side drawer created and implemented

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import Toolbar from '../navigation/toolbar/toolbar';
+import SideDrawer from '../side_drawer/side_drawer';
 import classes from './layout.module.css';
 interface Props {
 }
 const Layout: React.FunctionComponent<Props> = ({ children }) => (
     <Fragment>
         <Toolbar/>
+        <SideDrawer/>
         <main className={classes.mainContent}>
             {children}
         </main>
---
+++ src/components/side_drawer/side_drawer.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from '../logo/logo';
+import Nav from '../navigation/toolbar/nav/nav';
+
+interface SideDrawerProps{
+    show?: boolean;
+}
+const Wrapper = styled.div<SideDrawerProps>`
+    position: fixed;
+    left: 0;
+    top:0;
+    width: 280px;
+    max-width: 70%;
+    height: 100%;
+    background-color: white;
+    z-index: 200;
+    padding: 16px;
+`;
+const SideDrawer:React.FunctionComponent = (props)=>(
+    <Wrapper>
+        <div style={{height: "6%"}}>
+        <Logo/>
+        </div>
+        <br/>
+        <Nav/>
+    </Wrapper>
+);
+
+export default SideDrawer;
